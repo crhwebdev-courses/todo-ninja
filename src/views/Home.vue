2d6544bd4e80dbcc3f3edbd09030a9251dc76bda
Carl Hain <crhain@gmail.com>
2019-11-26 21:29:59 -0600

    Add several styles of buttons to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, possimus
-      cumque magni, excepturi voluptas neque dolore qui numquam culpa vel
-      eveniet sit quos aut officiis natus? Pariatur sit error voluptatem.
-    </p>
-    <p class="pink lighten-4 red--text text--darken-4">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium nemo
-      voluptas omnis, eveniet sed culpa, ratione ipsam molestiae laudantium,
-      deleniti consequuntur! Distinctio doloremque ipsam eos iste laborum
-      explicabo ratione optio.
-    </p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller display</h4>
-    <p class="headline">This is a headline</p>
-    <p class="subheading font-weight-bold">This is a subheading</p>
-    <p class="caption">This is a caption</p>
+    <div class="container">
+      <v-btn class="pink white--text mr-2">click me</v-btn>
+      <v-btn depressed color="pink">click me</v-btn>
+      <v-btn text color="pink">click me</v-btn>
+      <v-btn depressed class="pink white--text mr-2">
+        <v-icon left>mdi-email</v-icon>
+        <span>EMAIL ME</span>
+      </v-btn>
+      <v-btn small depressed class="pink white--text mr-2">
+        <v-icon left small>mdi-email</v-icon>
+        <span>EMAIL ME</span>
+      </v-btn>
+      <v-btn large depressed class="pink white--text mr-2">
+        <span>EMAIL ME</span>
+        <v-icon right large>mdi-email</v-icon>
+      </v-btn>
+      <v-btn fab small dark color="purple">
+        <v-icon small>mdi-heart</v-icon>
+      </v-btn>
+    </div>
   </div>
 </template>
 <script>
 export default {};
 </script>
+<style>
+.container {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+}
+</style>
