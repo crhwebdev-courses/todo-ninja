5f4c2ca8a2950ea9c071eae68aafe889d059f066
Carl Hain <crhain@gmail.com>
2019-11-26 20:55:53 -0600

    Play around with some different text styles and colors on the Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, possimus
+      cumque magni, excepturi voluptas neque dolore qui numquam culpa vel
+      eveniet sit quos aut officiis natus? Pariatur sit error voluptatem.
+    </p>
+    <p class="pink lighten-4 red--text text--darken-4">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium nemo
+      voluptas omnis, eveniet sed culpa, ratione ipsam molestiae laudantium,
+      deleniti consequuntur! Distinctio doloremque ipsam eos iste laborum
+      explicabo ratione optio.
+    </p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
+<script>
+export default {};
+</script>
