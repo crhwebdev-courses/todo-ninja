96b6098e4a227f72fc88118b83cb0470d1fd8abf
Carl Hain <crhain@gmail.com>
2019-11-27 12:33:37 -0600

    Add several buttons that are hidden at different breakpoints

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,9 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <div class="container">
-      <v-btn class="pink white--text mr-2">click me</v-btn>
-      <v-btn depressed color="pink">click me</v-btn>
-      <v-btn text color="pink">click me</v-btn>
-      <v-btn depressed class="pink white--text mr-2">
-        <v-icon left>mdi-email</v-icon>
-        <span>EMAIL ME</span>
-      </v-btn>
-      <v-btn small depressed class="pink white--text mr-2">
-        <v-icon left small>mdi-email</v-icon>
-        <span>EMAIL ME</span>
-      </v-btn>
-      <v-btn large depressed class="pink white--text mr-2">
-        <span>EMAIL ME</span>
-        <v-icon right large>mdi-email</v-icon>
-      </v-btn>
-      <v-btn fab small dark color="purple">
-        <v-icon small>mdi-heart</v-icon>
-      </v-btn>
-    </div>
+    <v-btn class="hidden-md-and-down">click me</v-btn>
+    <v-btn dark color="pink" class="hidden-md-and-up">click me</v-btn>
+    <v-btn dark color="blue" class="hidden-sm-only">click me</v-btn>
   </div>
 </template>
 <script>
