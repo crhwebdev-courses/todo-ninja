0b8dbcd20cf8ce9b5e411b1dda6407406857253a
Carl Hain <crhain@gmail.com>
2019-12-07 16:52:39 -0600

    Add some paragraph tags to each content section for Dashboard, Projects, and Team

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,10 +3,18 @@
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
 
     <v-container class="my-5">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-      necessitatibus velit similique iste eligendi quisquam soluta. Officiis
-      animi omnis dolore ipsa aliquam tempore, quam eius, assumenda qui, nisi
-      culpa eum.
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+        necessitatibus velit similique iste eligendi quisquam soluta. Officiis
+        animi omnis dolore ipsa aliquam tempore, quam eius, assumenda qui, nisi
+        culpa eum.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+        necessitatibus velit similique iste eligendi quisquam soluta. Officiis
+        animi omnis dolore ipsa aliquam tempore, quam eius, assumenda qui, nisi
+        culpa eum.
+      </p>
     </v-container>
   </div>
 </template>
