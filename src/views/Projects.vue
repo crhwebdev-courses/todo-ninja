29afdf5858550534370ca270e8d39aa137413b81
Carl Hain <crhain@gmail.com>
2019-12-07 16:53:55 -0600

    Remove paragraph tags and just use placeholder text for now

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -3,18 +3,7 @@
     <h1 class="subtitle-1 grey--text">Projects</h1>
 
     <v-container class="my-5">
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-        necessitatibus velit similique iste eligendi quisquam soluta. Officiis
-        animi omnis dolore ipsa aliquam tempore, quam eius, assumenda qui, nisi
-        culpa eum.
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-        necessitatibus velit similique iste eligendi quisquam soluta. Officiis
-        animi omnis dolore ipsa aliquam tempore, quam eius, assumenda qui, nisi
-        culpa eum.
-      </p>
+      content
     </v-container>
   </div>
 </template>
