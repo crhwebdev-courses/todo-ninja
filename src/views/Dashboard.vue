5011f1215974a2702393ab1cdd41e066d31c312c
Carl Hain <crhain@gmail.com>
2019-12-08 17:50:57 -0600

    Add two buttons to dashboard using a grid layout

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,14 @@
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
 
     <v-container class="my-5">
-      content
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-btn outlined block color="primary">1</v-btn>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
