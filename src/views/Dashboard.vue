b8bb924ba989494f67aa5a4cbddc1139bc13a1a2
Carl Hain <crhain@gmail.com>
2019-12-08 18:01:18 -0600

    Add second row of buttons to Dashboard using some custom flex box justification

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,11 +4,20 @@
 
     <v-container class="my-5">
       <v-row>
-        <v-col cols="12" md="6">
+        <v-col cols="12" lg="6" md="6">
           <v-btn outlined block color="primary">1</v-btn>
         </v-col>
-        <v-col cols="12" md="6">
+        <v-col cols="12" lg="6" md="6">
           <v-btn outlined block color="primary">2</v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row class="justify-space-around">
+        <v-col cols="4" md="3">
+          <v-btn outlined block color="success">1</v-btn>
+        </v-col>
+        <v-col cols="4" md="3">
+          <v-btn outlined block color="success">2</v-btn>
         </v-col>
       </v-row>
     </v-container>
@@ -17,10 +26,4 @@
 <script>
 export default {};
 </script>
-<style>
-.container {
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: center;
-}
-</style>
+<style></style>
