d653a866f6d17699527a7fd1afc2bc1beac13f84
Carl Hain <crhain@gmail.com>
2019-12-09 20:16:20 -0600

    Create grid layout for project listing on Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,23 +3,29 @@
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
 
     <v-container class="my-5">
-      <v-row>
-        <v-col cols="12" lg="6" md="6">
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-col>
-        <v-col cols="12" lg="6" md="6">
-          <v-btn outlined block color="primary">2</v-btn>
-        </v-col>
-      </v-row>
+      <v-card flat class="pa-3">
+        <v-row>
+          <v-col cols="12" md="6">
+            <div class="caption grey--text">Project Title</div>
+            <div>Create a new website</div>
+          </v-col>
 
-      <v-row class="justify-space-around">
-        <v-col cols="4" md="3">
-          <v-btn outlined block color="success">1</v-btn>
-        </v-col>
-        <v-col cols="4" md="3">
-          <v-btn outlined block color="success">2</v-btn>
-        </v-col>
-      </v-row>
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption grey--text">Person</div>
+            <div>The Net Ninja</div>
+          </v-col>
+
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption grey--text">Due By</div>
+            <div>1st January 2020</div>
+          </v-col>
+
+          <v-col cols="2" sm="4" md="2">
+            <div class="caption grey--text">Status</div>
+            <div>ongoing</div>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>
