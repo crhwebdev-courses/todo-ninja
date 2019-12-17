1387f9e4cca6fd026888128b3e77955e40d50edc
Carl Hain <crhain@gmail.com>
2019-12-17 16:03:13 -0600

    Add data to Team component

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -7,3 +7,18 @@
     </v-container>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      team: [
+        { name: "The Net Ninja", role: "Web developer" },
+        { name: "Ryu", role: "Graphic designer" },
+        { name: "Chun Li", role: "Web developer" },
+        { name: "Gouken", role: "Social media maverick" },
+        { name: "Yoshi", role: "Sales guru" }
+      ]
+    };
+  }
+};
+</script>
