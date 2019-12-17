6fa9978f713d00cb2d59ddbded5cef902376facc
Carl Hain <crhain@gmail.com>
2019-12-17 16:09:49 -0600

    Create basic layout for Team component

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -3,7 +3,21 @@
     <h1 class="subtitle-1 grey--text">Team</h1>
 
     <v-container class="my-5">
-      content
+      <v-row no-gutters>
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+          v-for="person in team"
+          :team="team"
+          :key="person.name"
+        >
+          <v-card flat class="text-center ma-3">
+            Hello
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
